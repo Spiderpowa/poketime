c0b78fc3cc3575f822ba36b882c43676005f126f
Jimmy Hu <jim2212001@gmail.com>
2019-03-17 20:06:03 +0800

    Updates

---
+++ precache-manifest.e9b9e1fa330ebc3c13325f42dc6f2a63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5225d8d9b413648b0476",
+    "url": "/static/css/main.f81a512e.chunk.css"
+  },
+  {
+    "revision": "5225d8d9b413648b0476",
+    "url": "/static/js/main.6d337f95.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ea134c800fdbbbfddba9",
+    "url": "/static/js/2.927a82f6.chunk.js"
+  },
+  {
+    "revision": "f10bf0780aecd8ff951259976c1784dd",
+    "url": "/static/media/pokeball.f10bf078.png"
+  },
+  {
+    "revision": "b07f556dfd155d9fe378e01d5faf635b",
+    "url": "/index.html"
+  }
+];
